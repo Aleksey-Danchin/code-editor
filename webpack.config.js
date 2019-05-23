0122d8f65b91f230fdabafeb4c7f0a375603cea7
Aleksey Danchin <aleksey.danchin@gmail.com>
2019-05-23 10:30:22 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	entry: {
+		app: './src/index.js'
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, '/dist'),
+		publicPath: '/dist'
+	},
+	module: {
+		rules: [
+			{
+				test: /.js$/,
+				loader: 'babel-loader',
+				exclude: '/node_modules/'
+			},
+			{
+				test: /\.css$/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+			}
+		]
+	},
+	devServer: {
+		overlay: true
+	},
+	plugins: [
+		new MonacoWebpackPlugin({
+			languages: ['javascript']
+		})
+	]
+}
